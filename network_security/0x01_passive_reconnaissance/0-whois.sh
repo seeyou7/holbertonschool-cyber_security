965b5aa7ee8324bf18d409d5d0603d1aad097354
seeyou7 <ysaberyo@gmail.com>
2024-12-10 07:11:04 -0500

    making the code in 2 line lenght

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '
-/^(Registrant|Admin|Tech)/ {
-    gsub(": ", ",");  # to Replace ": " with ","
-    gsub(", ", ",");  # to Remove spaces after commas
-    print $1 "," $2}' > "$1.csv"
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/ {gsub(": ", ","); gsub(", ", ","); print $1 "," $2}' > "$1.csv"
+
