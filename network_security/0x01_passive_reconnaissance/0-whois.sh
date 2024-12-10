7aaea9a42c85f9d64fbec26fb77ac4f4722b93f6
seeyou7 <ysaberyo@gmail.com>
2024-12-10 07:20:22 -0500

    rm the coma from end line

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/ {gsub(": ", ","); gsub(", ", ","); print $1 "," $2}' > "$1.csv"
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/ {gsub(": ", ","); gsub(", ", ","); print $1 "," $2 | "sed s/,$//"}' > "$1.csv"
+
