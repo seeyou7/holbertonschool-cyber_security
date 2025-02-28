1505f055cc017dff1f1e7718a40f9c2918c2d17f
seeyou7 <ysaberyo@gmail.com>
2025-02-28 17:11:22 -0500

    basic firewall rules that allows both new and ongoing SSH sessions  & bloc all others connection

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -P INPUT DROP && iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -P INPUT -j DROP && iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
