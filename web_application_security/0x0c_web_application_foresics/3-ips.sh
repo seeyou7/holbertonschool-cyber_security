bb01061194160c27c60ba24ab3613a7a3bb94036
seeyou7 <ysaberyo@gmail.com>
2025-04-09 17:58:23 -0400

    filter the log by uniq IP root user with success loging

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/bin/bash 
+#grep "Accepted password" auth.log | awk '{print $9}' | sort | uniq -c
 
-grep -i "accepted password" auth.log | awk '{print $(NF-3)}' | sort -u | wc -l
+#grep "Accepted password for root" auth.log | grep "from" | awk '{for (i=1;i<=NF;i++) if ($i=="from") print $(i+1)}'
+
+grep "Accepted password for root" auth.log | grep "from" | \
+awk '{for (i=1;i<=NF;i++) if ($i=="from") print $(i+1)}' | \
+grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | \
+sort -u | wc -l
+
